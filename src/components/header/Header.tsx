069b7c00253db23e8ef4dd5d7b7f315c473e445c
Andriikryl <andreyk557@gmail.com>
2023-10-06 19:18:24 +0300

    add about page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,39 @@ import React from "react";
 import { Container } from "../container/Container";
 import style from "./style.module.css";
 import Flag from "../flag/Flag";
+import Link from "next/link";
+
+const dataNav = [
+  {
+    id: 1,
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    href: "/about",
+  },
+];
 
 export default function Header() {
   return (
     <header className={style.header}>
       <Container>
         <div className={style.flex__group}>
-          <span className={style.header__title}>header</span>
+          <nav className={style.nav}>
+            <ul className={style.nav__list}>
+              {dataNav.map((item) => {
+                return (
+                  <li key={item.id} className={style.nav__item}>
+                    <Link className={style.nav__link} href={item.href}>
+                      {item.title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
           <Flag />
         </div>
       </Container>

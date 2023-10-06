47183a033cf90c94f3e730fc3fe1c6baf5053142
Andriikryl <andreyk557@gmail.com>
2023-10-06 19:07:03 +0300

    add flag component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Container } from "../container/Container";
 import style from "./style.module.css";
+import Flag from "../flag/Flag";
 
 export default function Header() {
   return (
     <header className={style.header}>
       <Container>
-        <div>
+        <div className={style.flex__group}>
           <span className={style.header__title}>header</span>
+          <Flag />
         </div>
       </Container>
     </header>
